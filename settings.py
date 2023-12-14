17a3727e3d55dc7f740bab44c041353521dcdced
abdelhakim54 <elouakouakabdelhakim@gmail.com>
2023-12-14 13:39:03 +0100

    add setings.py file containing all global variables

---
+++ settings.py
@@ -0,0 +1,9 @@
+# in this file we define all global variables that are accessed in all files in this scop
+
+
+
+def init():
+    global measures
+    measures = {"temperature":0,
+            "co2":1,
+            "humidity":2}
